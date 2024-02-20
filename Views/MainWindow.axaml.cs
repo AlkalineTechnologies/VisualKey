e630dc7a0482c94834a8ea9a8da801ce62eea88b
plasmarad <userofgogle356@gmail.com>
2024-02-19 21:13:37 -0500

    Improved gitignore, and  SplashScreen colors/design.

--- Views/MainWindow.axaml.cs
+++ Views/MainWindow.axaml.cs
@@ -1,12 +1,24 @@
+using Avalonia;
 using Avalonia.Controls;
-
+using Avalonia.Media;
 
 namespace visualkey.Views;
 
 public partial class MainWindow : Window
 {
+
     public MainWindow()
     {
         InitializeComponent();
+        var GradientBrush = new RadialGradientBrush {
+            GradientStops = new GradientStops {
+                new GradientStop(Color.Parse("#0b001f"), 0),
+                new GradientStop(Color.Parse("#5600f3"), 1.0),
+            },
+            // Center = new RelativePoint(1,1, RelativeUnit.Relative),
+            GradientOrigin = new RelativePoint(1,1, RelativeUnit.Relative),
+            Radius = 1.5
+        };
+        Background = GradientBrush;
     }
 }
